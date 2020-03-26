facb3b4667ecac9577ffca57dcf9f339359101c6
William Vitorino <williamvitorino3@gmail.com>
2020-03-26 16:58:00 -0300

    Funcionemento do Login.

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,35 +1,55 @@
-import React from "react"
-import { Link } from 'react-router-dom'
-
+import React, { useState } from "react"
+import { Link, useHistory} from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi'
 
 import './style.scss'
+import api from '../../services/api'
 import logo from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png'
 
-class Login extends React.Component {
+export default function Login () {
+	const [id, setId] = useState('');
 
-	render() {
-		return (
-			<div className="login-container">
-				<section className="form">
-					<img src={logo} alt={"Herois"} />
+	const history = useHistory();
 
-					<form>
-						<h1>Login:</h1>
-						<input placeholder="Sua ID" />
-						<button className="button" type="submit">Entrar</button>
+	function handleLogin(e) {
+		e.preventDefault();
 
+		api.post('sessions', { id }).then(
+			res => {
+				const { name } = res.data;
 
-						<Link className="back-link" to="/register">
-							<FiLogIn size={16} color="#e02041" /> Não tenho Cadastro
-						</Link>
-					</form>
-				</section>
-				<img src={heroesImg} alt={"Herois"} />
-			</div>
+				localStorage.setItem('ongId', id);
+				localStorage.setItem('ongName', name);
+
+				history.push('/profile')
+			}
+		).catch(
+			err => {
+				alert("Falha no Login.")
+			}
 		)
-	}
-}
 
-export default  Login
+	}
+
+	return (
+		<div className="login-container">
+			<section className="form">
+				<img src={logo} alt="Herois" />
+				<form onSubmit={handleLogin} >
+					<h1>Login:</h1>
+					<input
+						placeholder="Sua ID"
+						value={id}
+						onChange={e => { setId(e.target.value) }}
+					/>
+					<button className="button" type="submit">Entrar</button>
+					<Link className="back-link" to="/register">
+						<FiLogIn size={16} color="#e02041" /> Não tenho Cadastro
+					</Link>
+				</form>
+			</section>
+			<img src={heroesImg} alt={"Herois"} />
+		</div>
+	)
+}
