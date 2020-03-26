45107d6379040132108114aeddaf982eda9f189d
William Vitorino <williamvitorino3@gmail.com>
2020-03-26 20:28:17 -0300

    3106f20

---
+++ precache-manifest.974477cec4a05c6a71f6560da2a7f5cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9606aec0edd3843548b989082506751b",
+    "url": "/bethehero-web/index.html"
+  },
+  {
+    "revision": "e2c9e2a5184cc3d503e6",
+    "url": "/bethehero-web/static/css/main.c7ca183a.chunk.css"
+  },
+  {
+    "revision": "f91724a436fe4a648af2",
+    "url": "/bethehero-web/static/js/2.428b2465.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bethehero-web/static/js/2.428b2465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c9e2a5184cc3d503e6",
+    "url": "/bethehero-web/static/js/main.87acafd8.chunk.js"
+  },
+  {
+    "revision": "15729f1729d914293a93",
+    "url": "/bethehero-web/static/js/runtime-main.85b08af3.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/bethehero-web/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/bethehero-web/static/media/logo.7eea718b.svg"
+  }
+]);
