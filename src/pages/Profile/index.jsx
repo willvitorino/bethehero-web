e33d0fd0c5d00fa22b20136784ca7f7065ec79dc
William Vitorino <williamvitorino3@gmail.com>
2020-03-26 19:00:26 -0300

    Funcionalidade da página de Profile.

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,85 +1,107 @@
 import React from "react"
 import './style.scss'
+import api from "../../services/api";
 
 import logo from '../../assets/logo.svg'
 import { Link } from "react-router-dom";
 import { FiPower, FiTrash2 } from 'react-icons/fi'
 
-export default  function Profile () {
-	return (
-		<div className="profile-container">
-			<header>
-				<img src={logo} alt="Logo" />
-				<span>Bem vinda!</span>
+export default class Profile extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			ongName: localStorage.getItem('ongName'),
+			ongId: localStorage.getItem('ongId'),
+			incidents: []
+		}
+	};
 
-				<Link className="button" to="incidents/new">Cadastrar novo caso</Link>
-				<button>
-					<FiPower size={18} color="#e02041" />
-				</button>
-			</header>
+	componentDidMount() {
+		const Authorization = localStorage.getItem('ongId');
 
-			<h1> Casos Cadastrados </h1>
-			<ul>
-				<li>
-					<strong>Caso:</strong>
-					<p>Caso Teste</p>
+		if ( !Authorization ) {
+			this.props.history.replace('/')
+		} else {
+			api.get('profile', {
+				headers: {
+					Authorization
+				}
+			}).then(
+				res => {
+					const { ongName, ongId } = this.state;
+					this.setState({
+						ongName, ongId,
+						incidents: res.data
+					})
+				}
+			)
+		}
+	}
 
-					<strong>Descrição:</strong>
-					<p>Descrição Teste</p>
+	handleLogout = () => {
+		[ 'ongName', 'ongId' ].forEach( item => { localStorage.removeItem(item) } );
+		this.props.history.replace('/')
+	};
 
-					<strong>Valor:</strong>
-					<p>R$ 120,00</p>
+	handleDeleteIncident = (id) => {
+		api.delete(`incidents/${id}`, {
+			headers: {
+				Authorization: localStorage.getItem('ongId')
+			}
+		}).then(
+			() => {
+				const { ongName, ongId, incidents } = this.state;
+				this.setState({
+					ongName,
+					ongId,
+					incidents: incidents.filter( incident => incident.id !== id )
+				});
+			}
+		).catch(
+			err => {
+				alert(err.data.error)
+			}
+		)
+	};
 
-					<button>
-						<FiTrash2 size={20} color="#a8a8b3" />
+	render() {
+		const { ongName, incidents } = this.state;
+		return (
+			<div className="profile-container">
+				<header>
+					<img src={logo} alt="Logo" />
+					<span>Olá {ongName}.</span>
+
+					<Link className="button" to="incidents/new">Cadastrar novo caso</Link>
+					<button onClick={this.handleLogout} >
+						<FiPower size={18} color="#e02041" />
 					</button>
-				</li>
+				</header>
 
-				<li>
-					<strong>Caso:</strong>
-					<p>Caso Teste</p>
+				<h1> Casos Cadastrados </h1>
+				<ul>
+					{ incidents.map(
+						incident => (
+							<li key={incident.id} >
+								<strong>Caso:</strong>
+								<p>{ incident.title }</p>
 
-					<strong>Descrição:</strong>
-					<p>Descrição Teste</p>
+								<strong>Descrição:</strong>
+								<p>{ incident.description }</p>
 
-					<strong>Valor:</strong>
-					<p>R$ 120,00</p>
+								<strong>Valor:</strong>
+								<p>{ Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' }).format(incident.value) }</p>
 
-					<button>
-						<FiTrash2 size={20} color="#a8a8b3" />
-					</button>
-				</li>
-
-				<li>
-					<strong>Caso:</strong>
-					<p>Caso Teste</p>
-
-					<strong>Descrição:</strong>
-					<p>Descrição Teste</p>
-
-					<strong>Valor:</strong>
-					<p>R$ 120,00</p>
-
-					<button>
-						<FiTrash2 size={20} color="#a8a8b3" />
-					</button>
-				</li>
-
-				<li>
-					<strong>Caso:</strong>
-					<p>Caso Teste</p>
-
-					<strong>Descrição:</strong>
-					<p>Descrição Teste</p>
-
-					<strong>Valor:</strong>
-					<p>R$ 120,00</p>
-
-					<button>
-						<FiTrash2 size={20} color="#a8a8b3" />
-					</button>
-				</li>
-			</ul>
-		</div>
-	)
+								<button
+									onClick={ () => this.handleDeleteIncident(incident.id) }
+								>
+									<FiTrash2 size={20} color="#a8a8b3" />
+								</button>
+							</li>
+						)
+					) }
+				</ul>
+			</div>
+		)
+	}
 }
